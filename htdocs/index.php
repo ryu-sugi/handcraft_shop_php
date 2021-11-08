d0627d132ae44454a7760ccb5d03eab53ff19a09
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-11-08 20:45:06 +0900

    サンプル商品画像表示

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,24 +5,25 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
+  <link href="https://fonts.googleapis.com/css2?family=Philosopher&display=swap" rel="stylesheet"> <!-- google font -->
   <link rel="stylesheet" type="text/css" href="../stylesheets/html5reset-1.6.1.css">
   <title>Hand Craft Shop</title>
 </head>
 
 <body>
+<div id="container">
   <header class="header">
     <div class="heading">
         <h1>Hand Craft Shop</h1>
     </div>
     <div class="navbar">
-        <div class="search">アイテムを探す</div>
-        <div class="login">ログイン</div>
-        <div class="cart">カート</div>
-          <ul class="nav-list">
-            <li>リスト1</li>
-            <li>リスト2</li>
-            <li>リスト3</li>
-            <li>リスト4</li>
+        <a href="item.php" class="search">アイテムを探す</a>
+        <a href="login.php" class="login">ログイン</a>
+        <a href="cart.php" class="cart">カート</a>
+          <ul class="nav-list"> リスト
+            <li><a href="news.php">お知らせ</a></li>
+            <li><a href="comp.php">会社概要</a></li>
+            <li><a href="info.php">お問合せ</a></li>
           </ul>
     </div>
   </header>
@@ -33,18 +34,37 @@
         <img src="../phots/phot1.jpg" alt="メイン画像" class="phot1">
       </div>
     <!-- メイン情報を表示する -->
+    <div class="sample-phot">
+      <div class="sample-phot1">
+        <img src="../phots/phot2.jpg" alt="sample-phot1" width="200px" height="200px" class="sample-phot1">
+        <br> サンプル商品1 <br>
+        ¥500
+      </div>
+      <div class="sample-phot2">
+        <img src="../phots/phot3.jpg" alt="sample-phot2" width="200px" height="200px" class="sample-phot2">
+        <br> サンプル商品2 <br>
+        ¥800
+      </div>
+      <div class="sample-phot3">
+        <img src="../phots/phot4.jpg" alt="sample-phot3" width="200px" height="200px" class="sample-phot3">
+        <br> サンプル商品3 <br>
+        ¥1000
+      </div>
+    </div>
+
+
     </div>
     <div class="second-content">
     <!-- 商品画像を表示する -->
     </div>
   </div>
 
-  <footer class="footer">
+  <div id="footer">
     <div class="footer-content">
       Author is Ryuji Sugita.
     </div>
-  </footer>
-
+  </div>
+  </div>
 <script type="text/javascript" src="../js/shop.js"></script>
 </body>
 </html>
