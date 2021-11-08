fedfc2bed3b588c9213254c49fb67b1694176f3e
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-11-08 15:52:58 +0900

    トップページ配置決定

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Hand Craft Shop</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
+  <link rel="stylesheet" type="text/css" href="../stylesheets/html5reset-1.6.1.css">
+  <title>Hand Craft Shop</title>
 </head>
-<body>
 
+<body>
+  <header class="header">
+    <div class="heading">
+        <h1>Hand Craft Shop</h1>
+    </div>
+    <div class="navbar">
+        <div class="search">アイテムを探す</div>
+        <div class="login">ログイン</div>
+        <div class="cart">カート</div>
+          <ul class="nav-list">
+            <li>リスト1</li>
+            <li>リスト2</li>
+            <li>リスト3</li>
+            <li>リスト4</li>
+          </ul>
+    </div>
+  </header>
+
+  <div class="main-content">
+    <div class="first-content">
+      <div class="first-view">
+        <img src="../phots/phot1.jpg" alt="メイン画像" class="phot1">
+      </div>
+    <!-- メイン情報を表示する -->
+    </div>
+    <div class="second-content">
+    <!-- 商品画像を表示する -->
+    </div>
+  </div>
+
+  <footer class="footer">
+    <div class="footer-content">
+      Author is Ryuji Sugita.
+    </div>
+  </footer>
+
+<script type="text/javascript" src="../js/shop.js"></script>
 </body>
 </html>
