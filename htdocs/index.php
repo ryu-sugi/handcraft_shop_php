7f9c1fac0390e120e8304882f003b043cc56b811
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-11-12 13:22:08 +0900

    headerを部分パーツとして分割
    include関数を使用

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,33 +11,9 @@
 </head>
 
 <body>
-<div id="container">
-  <header class="header">
-    <div class="heading">
-     <a href="index.php"> <h1>Hand Craft Shop</h1> </a>
-    </div>
-    <div class="navbar">
-      <a href="item.php" class="search">アイテムを探す</a>
-      <a href="login.php" class="login">ログイン</a>
-      <a href="cart.php" class="cart">カート</a>
-    </div>
-    <div class="sp-menu">
-      <!-- Webフォントから取り出したアイコン -->
-      <span class="material-icons" id="open">menu</span>
-      <div class="overlay">
-        <span class="material-icons" id="close">close</span>
-        <nav class="hamburger-nav">
-          <ul class="nav-list">
-            <li><a href="news.php">お知らせ</a></li>
-            <li><a href="comp.php">会社概要</a></li>
-            <li><a href="info.php">お問合せ</a></li>
-          </ul>
-        </nav>
-      </div>
-      </div>
-    </header>
 
-    
+<?php include('./parts/header.html'); ?>
+
   <div class="main-content">
     <div class="first-content">
       <div class="first-view">
